34336d75e9a6c101bee5f55edc316d61cde9c1d8
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-09-16 17:22:50 -0300

    Update: Modularizar funciones en controllers de heroes

--- controllers/heroeController.js
+++ controllers/heroeController.js
@@ -1,12 +1,27 @@
 const scienceData = require('../db/science.js');
 
+function obtenerIdYNombre(array) {
+    return array.map(heroe => ({
+        id: heroe.id,
+        nombre: heroe.nombre
+    }))
+}
+
+function buscarHeroe(idHeroe) {
+    return scienceData.lista.find(h => h.id == idHeroe);
+}
+
+const lista = obtenerIdYNombre(scienceData.lista);
+
 const heroeController = {
     index:(req, res, next) => {
-        res.render('heroes/list', { title: 'Heroes de la programación', data : scienceData.lista });
+        res.render('heroes/list', { 
+            title: 'Heroes de la programación', 
+            data : lista });
     },
     showDetail: (req, res, next) => {
         id = req.params.id;
-        heroe = scienceData.lista.find(h => h.id == id);
+        heroe = buscarHeroe(id);
 
         if (!heroe) {
             return res.status(404).send('No encontramos al científico indicado. Por favor, elija otro id');
@@ -22,7 +37,7 @@ const heroeController = {
     },
     showBio: (req, res, next) => {
         id = req.params.id;
-        heroe = scienceData.lista.find(h => h.id == id);
+        heroe = buscarHeroe(id);
         isOk = req.params.ok;
 
         if (!heroe) {
